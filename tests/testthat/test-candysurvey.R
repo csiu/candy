9472a90e29287555f41d8b13e100c385cf87a5d1
csiu <celia.sy.siu@gmail.com>
2015-11-20 21:54:35 -0800

    add some tests

--- tests/testthat/test-candysurvey.R
+++ tests/testthat/test-candysurvey.R
@@ -1,8 +1,22 @@
-context("candysurvey")
+context("Validity of candysurvey data")
 
-## TODO: Rename context
-## TODO: Add more tests
+suppressPackageStartupMessages(library(dplyr))
+find_int <- function(userID, field)
+  candysurvey %>% filter(user == userID) %>% select_(field) %>% .[[1]]
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ages are not screwed up when convert to factor and back to integer", {
+  expect_equal(find_int("ID-0138", "age"), 35)
+  expect_equal(find_int("ID-0150", "age"), 33)
+  expect_equal(find_int("ID-0146", "age"), NA_integer_)
+})
+
+test_that("n_mints are not screwed up when convert to factor and back to integer", {
+  expect_equal(find_int("ID-0138", "n_mints"), 2)
+  expect_equal(find_int("ID-0150", "n_mints"), 5)
+  expect_equal(find_int("ID-0199", "n_mints"), NA_integer_)
+})
+
+test_that("trick_or_treat is boolean", {
+  expect_false(find_int("ID-0138", "trick_or_treat"))
+  expect_true(find_int("ID-0146", "trick_or_treat"))
 })
