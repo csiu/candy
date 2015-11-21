2ddfe339cab733f35d19de070e8f3fb892787f05
csiu <celia.sy.siu@gmail.com>
2015-11-20 21:25:59 -0800

    init testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(candysurvey)
+
+test_check("candysurvey")
---
+++ tests/testthat/test-candysurvey.R
@@ -0,0 +1,8 @@
+context("candysurvey")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
